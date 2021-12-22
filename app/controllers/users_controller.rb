8195b33a616769c880feb0896ad2527172b96030
EC2 Default User <ec2-user@ip-172-31-0-111.ec2.internal>
2021-12-22 05:04:54 +0000

    Finish2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,26 +3,13 @@ class UsersController < ApplicationController
   def index
     @user = current_user
     @users = User.all
-    @book_new = Book.new
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
-    @book_new = Book.new
+    @book = Book.new
     @books = @user.books.all
-  end
-
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
-    else
-      @user = current_user
-      @books = Book.all
-      render 'books/index'    
-    end
   end
 
   def edit
