cffd472fa33062ce4fad8887e32c8e94b7959341
EC2 Default User <ec2-user@ip-172-31-0-111.ec2.internal>
2021-12-25 12:43:44 +0000

    Application_issu3.finish

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
-  
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true
   validates :body, length: {in: 1..200}, if: proc { |s| s.body.present? }
-  
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'home/about' =>'home#about', as: 'about'
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :create, :show, :edit, :update]
 end
