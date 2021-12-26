30ec2eed01734ae1fea33783925fc810e4aaf249
EC2 Default User <ec2-user@ip-172-31-0-111.ec2.internal>
2021-12-26 09:24:55 +0000

    Finish.application_issue4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'home/about' =>'home#about', as: 'about'
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' =>'home#about', as: 'about'
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :create, :show, :edit, :update]
+  resources :users, only: [:index, :create, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
